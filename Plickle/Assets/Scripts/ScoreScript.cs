749dee39adc5ad824cbd424461ae8e04814a1a24
boydensuperstar <jeremybwarner@gmail.com>
2019-03-10 20:00:56 -0700

    add materials fixed force

---
+++ Plickle/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour
+{
+    // once the pickle hits the koolaid, you score a point
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Score!");
+    }
+}
